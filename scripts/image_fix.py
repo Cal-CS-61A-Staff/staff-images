74a7e9060b3a639b224932ac939189a5177c0560
Laryn Qi <larynqi@berkeley.edu>
2020-06-19 21:30:09 -0700

    style

--- scripts/image_fix.py
+++ scripts/image_fix.py
@@ -9,6 +9,12 @@ python image_fix.py <semester>
 
 e.g. python image_fix.py su20
 
+---NOTES---
+
+- this script will override the original image files
+
+- since the cropping is very naive, it can be lead to unintuitive results
+
 TODO:
 - allow for adjustment if the crop doesn't look good
 """
@@ -21,16 +27,15 @@ for file in images:
     if img.width != img.height:
         size = min(img.width, img.height)
         result = SimpleImage.blank(size, size)
-        crop_x = 0
-        crop_y = 0
-        if img.width < img.height:
-            crop_y = (img.height - img.width) / 2
-        else:
+        crop_x = crop_y = 0
+        if img.width > img.height:
             crop_x = (img.width - img.height) / 2
+        else:
+            crop_y = (img.height - img.width) / 2
         for x in range(size):
             for y in range(size):
-                old_y = y + crop_y
                 old_x = x + crop_x
+                old_y = y + crop_y
                 old_pixel = img.get_pixel(old_x, old_y)
                 result.set_pixel(x, y, old_pixel)
         result.pil_image.save(PATH + file)
